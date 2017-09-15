47ce2b6112e0fc5c6350a4453557c136d472a8a8
Christos Savvopoulos <savvopoulos@gmail.com>
2017-09-15 16:06:42 +0100

    With webpack.

---
+++ build.sh
@@ -0,0 +1,1 @@
+pip3 install aiohttp && npm install && ./node_modules/.bin/webpack && cp index.html dist/
--- serve.py
+++ serve.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 from aiohttp import web
 
 app = web.Application()
 app.router.add_static('/', './')
+app.router.add_static('/dist', './dist')
 web.run_app(app, port=8000)
